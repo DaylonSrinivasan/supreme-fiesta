09cd92c74bff7771868b37814b3069e5b897dc1c
Daylon Srinivasan <daylon.srinivasan@gmail.com>
2016-07-02 23:15:50 -0700

    first image

--- index.php
+++ index.php
@@ -21,6 +21,37 @@
       </script>
 
 
+      <img id="animation" width="220" height="277"
+      src="animation.png" hidden>
+
+
+      <canvas id="myCanvas" width="320" height="320"
+      style="border:1px solid #d3d3d3;" onmousedown = "mouseDown()" onmouseup="mouseUp()">
+      Your browser does not support the HTML5 canvas tag.
+      </canvas>
+      
+      <script>
+
+      var xpos = 0;
+      var canvas = document.getElementById("myCanvas");
+      var ctx = canvas.getContext("2d");
+      var img = document.getElementById("animation");
+
+      ctx.drawImage(img, xpos, 0, 320, 320, 0, 0, 320, 320);
+
+      function mouseDown() {
+        xpos+=320;
+        if(xpos>320*6)
+          xpos = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, xpos, 0, 320, 320, 0, 0, 320, 320);
+
+      }
+
+      function mouseUp() {
+      //do something
+      }
+
 
 
 
