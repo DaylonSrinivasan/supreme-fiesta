a274ede4585ea4588d01650325a6c19f6e415597
Daylon Srinivasan <daylon.srinivasan@gmail.com>
2016-07-02 20:42:00 -0700

    initial php/js tests

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <html>
-<head> <title> Let's do this </title> </head>
-<body> America </body>
+  <head>
+     <title> Let's do this </title>
+   </head>
+   <body> America
+     <?php
+        echo "Testing php";
+        $var = 1;
+        while($var < 5){
+          echo $var . "\n";
+          $var+=1;
+        }
+      ?>
+
+      <script>
+        console.log("Testing javascript");
+        var hello = "world";
+        console.log(hello);
+      </script>
+        
+
+   </body>
 </html>
