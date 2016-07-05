c3833cdeb51604a8e8334c17848f6730bdef0d67
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2016-07-04 18:00:25 -0700

    check

--- js/cheats.js
+++ js/cheats.js
@@ -1,7 +1,7 @@
 $("#submit").click(function() {
   var userInput = $("#inputtext").val();
   $.ajax({
-    url:  'https://daylon-and-jocelyn.herokuapp.com/secret.php',
+    url:  'http://daylon-and-jocelyn.herokuapp.com/secret.php',
     type: 'POST',
     data: {inputtext: userInput},
     success: function(response) {
