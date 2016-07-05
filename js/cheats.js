8ae432511128730aac9491b100ef5a109107a9c4
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2016-07-04 17:58:46 -0700

    removed "php" folder

--- js/cheats.js
+++ js/cheats.js
@@ -1,7 +1,7 @@
 $("#submit").click(function() {
   var userInput = $("#inputtext").val();
   $.ajax({
-    url:  'https://daylon-and-jocelyn.herokuapp.com/php/secret.php',
+    url:  'https://daylon-and-jocelyn.herokuapp.com/secret.php',
     type: 'POST',
     data: {inputtext: userInput},
     success: function(response) {
