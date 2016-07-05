e637879df581a3aacbd6375f710bb91e455717d7
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2016-07-05 01:06:56 -0700

    Update secret.php

--- secret.php
+++ secret.php
@@ -4,9 +4,8 @@
   $ans = $_POST['inputtext'];
   if($ans==="3fast5me")
     echo "speed";
-  if($ans==="jocelyn&daylon<3")
+  else if($ans==="jocelyn&daylon<3")
     echo "fishheart";
-    break;
   else
     echo "failure";
 
