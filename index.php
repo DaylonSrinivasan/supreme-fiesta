a3c06c1e5ca2da1c91a31237da59e8ec9b059dbf
Jocelyn Tran <jocelyn.tran1@gmail.com>
2016-09-15 03:53:27 -0700

    added some A1 styling

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+
+    <!-- Font -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+
+  <!-- Styles -->
+    <link href="./style.css" rel="stylesheet" media="screen">
+
+</head>
+
 <body>
   <!-- dependencies -->
 
@@ -17,9 +28,9 @@
   <img id="bubble" width="32" height="32" src="images/bubble.png" hidden>
   <img id="background" src="images/background2.jpg" hidden>
 
+  <h1>A FISHY GAME</h1>
 
-
-  <canvas id="myCanvas" style="width:100%" width="1000" height = "400">
+  <canvas id="myCanvas" style="width:100%" width="1000" height = "350">
   style="border:1px solid #d3d3d3;">
   Your browser does not support the HTML5 canvas tag.
   </canvas>
@@ -38,6 +49,10 @@
   <br>
   <div>
     <p style="float: center" id="result"></p>
+  </div>
+
+  <div>
+  <p>Made by Daylon Srinivasan and Jocelyn Tran. See our code on <a href="https://github.com/DaylonSrinivasan/supreme-fiesta" target="_blank" style="text-decoration: underline;">GitHub.</a></p>
   </div>
 
   <!-- include javascript files -->
